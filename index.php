6b8357b38640c23957b16e775c7bb3279c6da248
AjayDextrous <ajay.narayanan.zx@gmail.com>
2016-07-31 10:21:11 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,56 @@
+ <!DOCTYPE html>
+<html>
+<!--Title and CSS-->
+	<head>
+		<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+		<meta name="Ajay Narayanan" content="Atmos Sensor Network" />
+		<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+	  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+	  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+		<title>The ATMOS Sensor Network</title>
+	</head>
+<!--Header stuff-->
+	<body>
+		<nav class="navbar navbar-default">
+		  <div class="container-fluid">
+		    <div class="navbar-header">
+		      <a class="navbar-brand" href="#">ATMOS</a>
+		    </div>
+		    <ul class="nav navbar-nav">
+		      <li class="active"><a href="#">Home</a></li>
+		      <li><a href="/node_list/index.php">Nodes</a></li>
+		      <li><a href="/php_info/index.php">About</a></li>
+		      <li><a href="/contact_us/index.php">Contact</a></li>
+		    </ul>
+		  </div>
+		</nav>
+
+		<!DOCTYPE html>
+<html lang="en">
+
+
+<div class="jumbotron text-center">
+  <h1>The ATMOS Sensor Network</h1>
+  <p>A joint project by the IET CET and the IIITM-K to map our environment</p> 
+</div>
+  
+<div class="container">
+  <div class="row">
+    <div class="col-sm-4">
+      <h3>Nodes</h3>
+      <p>View the list of nodes in service</p>
+    </div>
+    <div class="col-sm-4">
+      <h3>Sensor Values</h3>
+      <p>Sensor Values are graphed in real time</p>
+    </div>
+    <div class="col-sm-4">
+      <h3>Maps</h3>        
+      <p>Values are mapped for easy use by researchers</p>
+
+    </div>
+  </div>
+</div>
+	
+	</body>
+</html> 
