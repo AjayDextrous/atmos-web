0bafdad7233d99651cd5300fa828c9c936143f8f
AjayDextrous <ajay.narayanan.zx@gmail.com>
2016-07-31 10:28:57 +0530

    Create index.php

---
+++ contact_us/index.php
@@ -0,0 +1,30 @@
+ <!DOCTYPE html>
+<html>
+<!--Title and CSS-->
+	<head>
+		<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+		<meta name="Ajay Narayanan" content="Atmos Sensor Network" />
+		<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+	  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+	  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+		<title>ATMOS v0.1</title>
+	</head>
+<!--Header stuff-->
+	<body>
+		<nav class="navbar navbar-default">
+		  <div class="container-fluid">
+		    <div class="navbar-header">
+		      <a class="navbar-brand" href="#">ATMOS</a>
+		    </div>
+		    <ul class="nav navbar-nav">
+		      <li><a href="/index.php">Home</a></li>
+		      <li><a href="/node_list/index.php">Nodes</a></li>
+		      <li><a href="/php_info/index.php">About</a></li>
+		      <li class="active"><a href="#">Contact</a></li>
+		    </ul>
+		  </div>
+		</nav>
+		
+	
+	</body>
+</html> 
