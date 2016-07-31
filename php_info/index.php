501a4279510ec0df84116ba01f6afc8028f0be82
AjayDextrous <ajay.narayanan.zx@gmail.com>
2016-07-31 10:25:36 +0530

    Create index.php

---
+++ php_info/index.php
@@ -0,0 +1,13 @@
+ <!DOCTYPE html>
+<html>
+<!--Title and CSS-->
+  <head>
+	  <meta http-equiv="content-type" content="text/html;charset=utf-8" />
+	  <meta name="Ajay Narayanan" content="Atmos Sensor Network" />
+	  <title>ATMOS v0.1</title>
+  </head>
+<!--Header stuff-->
+  <body>
+    <?php phpinfo();?>
+  </body>
+</html> 
