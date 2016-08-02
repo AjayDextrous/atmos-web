2de1155368cd6c98d9baa006266f3cb96c99c0c4
AjayDextrous <ajay.narayanan.zx@gmail.com>
2016-08-02 10:36:10 +0530

    Add files via upload
    working code v.1

--- send_node_data/node.php
+++ send_node_data/node.php
@@ -1,4 +1,9 @@
  <!DOCTYPE html>
+	<html>
+	<head>
+		<title></title>
+	</head>
+	<body>
 	<?php 
 	$dtime		=	$_GET["dtime"];
 	$temp		=	$_GET["temp"];
@@ -13,13 +18,18 @@
    	if(! $conn ) {
     	die('Could not connect: ' . mysql_error());
    	}
+   	else echo "Connected successfully <br>";
 
   	 mysql_select_db('atmos');
-  	 $sql = "INSERT INTO node.$node_ID (temp,hum,dtime) VALUES ($temp,$hum,$dtime)";
-  	 if ($conn->query($sql) === TRUE) {
+  	 $sql = "INSERT INTO node$node_ID (temp,hum,dtime) VALUES ($temp,$hum,$dtime)";
+  	 echo "query is $sql <br>";
+  	 if (mysql_query($sql) === TRUE) {
     	echo "New record created successfully";
 	} 
 		else {
-    		echo "Error: " . $sql . "<br>" . $conn->error;
+    		echo "Error: " . $sql . "<br>" . mysql_error();
 		}
 	?>
+	</body>
+	</html>
+	
