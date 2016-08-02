d23fde670568072efb577fc42bc524b5215da71a
AjayDextrous <ajay.narayanan.zx@gmail.com>
2016-08-02 11:26:43 +0530

    Add files via upload
    Working code v1

--- node_view/index.php
+++ node_view/index.php
@@ -7,18 +7,53 @@
 		<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
 	  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 	  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-		<title>The ATMOS Sensor Network</title>
+		<title>ATMOS v0.1</title>
 	</head>
 <!--Header stuff-->
-	<div class="container">
-  <h2>Sensor Readings</h2>
-  <p>Node <?php echo $_GET["node_id"] ?></p>            
+	<body>
+	<?php 
+	$node_ID	=	$_GET["nodeID"];
+	?>
+		<nav class="navbar navbar-default">
+		  <div class="container-fluid">
+		    <div class="navbar-header">
+		      <a class="navbar-brand" href="#">ATMOS</a>
+		    </div>
+		    <ul class="nav navbar-nav">
+		      <li><a href="/index.php">Home</a></li>
+		      <li class="active"><a href="#">Nodes</a></li>
+		      <li><a href="/php_info/index.php">About</a></li>
+		      <li><a href="/contact_us/index.php">Contact</a></li>
+		    </ul>
+		  </div>
+		</nav>
+		
+		<!--Main PHP Work Here-->
+		<div class="container">
+  <h2>Node <?php echo $node_ID; ?></h2>
+  <p>Sensor values for  
+  			<?php
+			$node = $_POST['node'];
+		   	$password = $_POST['password'];
+
+		   	$dbhost = 'localhost:3036';
+		   	$dbuser = 'atmos';
+		   	$dbpass = '1ndulals';
+		   	$conn = mysql_connect($dbhost, $dbuser, $dbpass);
+		   	if(! $conn ) {
+		    	die('Could not connect: ' . mysql_error());
+		   	}
+		  	 mysql_select_db('atmos');
+		  	 $sql = "SELECT node_name FROM nodelist WHERE node_id = \"node".$node_ID."\"";
+		  	 $retnode = mysql_fetch_assoc(mysql_query($sql));
+		  	 echo $retnode["node_name"];
+		  	 ?></p>            
   <table class="table table-striped">
     <thead>
       <tr>
-        <th>Node ID</th>
-        <th>Node Name</th>
-        <th>Status</th>
+        <th>Time</th>
+        <th>Temperature</th>
+        <th>Humidity</th>
       </tr>
     </thead>
     <tbody>
@@ -34,11 +69,11 @@
 		    	die('Could not connect: ' . mysql_error());
 		   	}
 		  	 mysql_select_db('atmos');
-		   	$sql = "SELECT * FROM $_GET['node_name']";
+		   	$sql = "SELECT * FROM node".$node_ID;
 		 	$retval = mysql_query($sql);
 			while($row = mysql_fetch_array($retval))
 		          {
-		           echo "<tr><td>" . $row['node_id'] . "</td><td> " . $row['node_name'] . "</td><td>" .$row['node_status']."</td></tr>"; 
+		           echo "<tr><td>" . $row['dtime'] . "</td><td> " . $row['temp'] . "</td><td>" .$row['hum']."</td></tr>"; 
 		           }
 
 			mysql_close($conn);
@@ -46,7 +81,10 @@
 		 </tbody>
   </table>
 </div>
-	<body>
-	
+		<!--Footer-->
+				<div id="footer">
+					<p>Copyright &copy; <em>IET-CET</em> &middot;</p>
+				</div>	
+			</div>
 	</body>
 </html> 
